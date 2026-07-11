a0be016198d476907cc325e5ccfc172d8b9bdd55
mohamedelieba <mohamed.eliba712@gmail.com>
2026-07-12 00:28:37 +0300

    LLM Interface and factory for embedding and text generation

---
+++ src/stores/llm/LLMEnums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class LLMEnums(Enum):
+    
+    OPENAI = "OPENAI"
+    COHERE = "COHERE"
+
+class OpenAIEnum(Enum):
+
+    SYSTEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
+class CoHereEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+class DocumetTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
