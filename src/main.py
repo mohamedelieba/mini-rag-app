c63dfb76219e6b43730459fd03212399822e2abb
mohamedelieba <mohamed.eliba712@gmail.com>
2026-07-13 00:53:37 +0300

    creating the vectordb client and fixing some issues

--- src/main.py
+++ src/main.py
@@ -4,17 +4,19 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 app = FastAPI()
 
 
-async def startup_db_client():
+async def startup_span():
     
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URI)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
+    vector_provider_factory = VectorDBProviderFactory(settings)
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
@@ -24,13 +26,20 @@ async def startup_db_client():
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, 
                                                embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    # vector db client
+    app.vectordb_client = vector_provider_factory.create(
+        provider=settings.VECTOR_DB_BACKEND
+    )
+
+    app.vectordb_client.connect()
 
 
-async def shutdown_db_client():
+async def shutdown_span():
     app.mongo_conn.close()
-
-app.router.lifespan.on_startup.append(startup_db_client)
-app.router.lifespan.on_startup.append(shutdown_db_client)
+    app.vectordb_client.disconnect()
+    
+app.router.lifespan.on_startup.append(startup_span)
+app.router.lifespan.on_shutdown.append(shutdown_span)
 
 
 app.include_router(base.base_router)
